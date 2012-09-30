11c287ef0a9a5e02445c8654f52a91f667eac530
Martin Kolman <martin.kolman@gmail.com>
2012-09-30 18:49:45 +0200

    cleanup

--- gui/qml_gui.py
+++ gui/qml_gui.py
@@ -209,29 +209,9 @@ class Panora(QObject):
     lastFolder = self.panora.get('lastChooserFolder', defaultPath)
     return lastFolder
 
-  @QtCore.Slot(str, result=str)
-  def storeNewAsOld(self, capturedImagePath):
-    """
-    store an image captured by Panora
-    as if it was a normally loaded "old" image
-
-    * move it from the camera capture file
-    * set paths as if it was loaded from a file/url/gallery
-    """
-    dateString = str(int(time.time()))
-    filename = "rp" + dateString + ".jpg"
-    self.oldImageFilename = filename
-    folder = "/home/user/MyDocs/pictures"
-    self.currentFolder = folder
-
-    storagePath = os.path.join(folder, filename)
-    shutil.move(capturedImagePath, storagePath)
-    self.gui._notify("Saved as:<br><b>%s</b>" % storagePath)
-    return storagePath
-
   @QtCore.Slot(str, str, int, result=str)
   def storeImage(self, capturedImagePath, projectName, index):
-    newFilename = "%s_panora_%s.jpg" % (self.oldImageFilename, dateString)
+    newFilename = "%s_%s.jpg" % (projectName, index)
     folder = self.mieru.platform.getDefaultPhotoStoragePath()
     storagePath = os.path.join(folder, newFilename)
 
