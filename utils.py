29e16674a34c27733e92547e1e04b3210587741c
Martin Kolman <martin.kolman@gmail.com>
2012-09-30 19:26:44 +0200

    cleanup

--- utils.py
+++ utils.py
@@ -1,11 +1,1 @@
 """various miscellaneous utility functions for Panora"""
-
-class Point:
-    def __init__(self, x, y):
-        self.x, self.y = x, y
-
-    def add(self, other):
-        return Point(self.x + other.x, self.y + other.y)
-
-    def __str__(self):
-        print "(%d, %d)" % (self.x, self.y)
